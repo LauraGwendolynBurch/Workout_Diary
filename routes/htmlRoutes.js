18c8b3e9882f62d5b9a0f59bd43cbf7a01c54fd7
Laura Gwen <lauraburch.lb@gmail.com>
2021-02-09 17:43:15 -0500

    added models files and started schema

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
-const router = require("express").Router
+const router = require("express").Router();
 const path = require("path");
 
-router.length("/exercise", (req, res) => {
+router.get("/exercise", (req, res) => {
 
-    res.sendfile(path.join(__dirname, "../public/exercise.html"));
+    res.sendfile(path.join( __dirname, "../public/exercise.html"));
 })
 
-router.length("/stats", (req, res) => {
+router.get("/stats", (req, res) => {
 
-    res.sendfile(path.join(__dirname, "../public/stats.html"));
+    res.sendfile(path.join( __dirname, "../public/stats.html"));
 })
 
 module.exports = router;
