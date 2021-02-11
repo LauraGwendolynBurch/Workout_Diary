c28e7059b9c80b9229e78f0af66691981835d299
Laura Gwen <lauraburch.lb@gmail.com>
2021-02-10 21:28:05 -0500

    fixed schema and put routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { Workout } = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-    
+
     Workout.find().then((workouts) => {
-console.log(workouts)
+        console.log(workouts)
         res.json(workouts);
 
     }).catch((err) => {
@@ -16,15 +16,15 @@ console.log(workouts)
 
 
 router.post("/api/workouts", (req, res) => {
-    console.log(req.body)
-    Workout.create( req.body ).then((workout) => {
+    
+    Workout.create(req.body).then((workout) => {
 
         res.json(workout);
 
     }).catch((err) => {
 
         res.status(500).json(err);
-        
+
     });
 
 });
@@ -32,23 +32,39 @@ router.post("/api/workouts", (req, res) => {
 router.put("/api/workouts/:id", (req, res) => {
 
     const newExercise = req.body;
-    console.log(newExercise)
-    Workout.findByIdAndUpdate( req.params.id, { $push: {
-        exercises: newExercise 
-    }} ).then((workout) => {
+   
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: newExercise
+        },
+       
+    },
+    {
+        new:true,
+    }).then((workout) => {
 
         res.json(workout);
 
     }).catch((err) => {
 
         res.json(err);
-        
+
     });
 
 });
 
 router.get("/api/workouts/range", (req, res) => {
 
+
+    Workout.find().limit(7).then((workouts) => {
+        console.log(workouts)
+        res.json(workouts);
+
+    }).catch((err) => {
+
+        res.status(500).json(err);
+
+    });
 
 
 });
